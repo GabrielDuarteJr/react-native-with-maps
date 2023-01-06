07f69e022ae9672dc68666188f8db9ed795c47ac
Gabriel Duarte Jr <gabriel181193@gmail.com>
2023-01-05 23:54:43 -0300

    chore: navigation script created

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,33 +1,69 @@
-import React from 'react';
-import MapView from 'react-native-maps';
-import {StyleSheet, View} from 'react-native';
+import React, {useEffect, useState, useMemo} from 'react';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+import VehicleLocationMock from '~/utils/mocks/VehicleLocationMock';
+
+const GOOGLE_MAPS_APIKEY = '######################';
 
 const HomeScreen = () => {
+  const [origin, setOrigin] = useState(VehicleLocationMock[0]);
+  const [originItemIndex, setOriginItemIndex] = useState(0);
+
+  const destination = {
+    latitude: -18.920112,
+    longitude: -48.242071,
+  };
+
+  const originLoc = useMemo(
+    () => ({
+      latitude: origin.latitude,
+      longitude: origin.longitude,
+    }),
+    [origin],
+  );
+
+  const destinationLoc = {
+    latitude: destination.latitude,
+    longitude: destination.longitude,
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (originItemIndex < VehicleLocationMock.length) {
+        const newOrigin = VehicleLocationMock[originItemIndex];
+        setOrigin(newOrigin);
+        setOriginItemIndex(originItemIndex + 1);
+      } else {
+        clearInterval(interval);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [originItemIndex]);
+
   return (
-    <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}
+    <MapView
+      style={{width: '100%', height: '100%'}}
+      provider={PROVIDER_GOOGLE}
+      showsUserLocation={true}
+      initialRegion={{
+        latitude: -18.920112,
+        longitude: -48.242071,
+        latitudeDelta: 0.0222,
+        longitudeDelta: 0.0121,
+      }}>
+      <MapViewDirections
+        origin={originLoc}
+        destination={destinationLoc}
+        apikey={GOOGLE_MAPS_APIKEY}
+        strokeWidth={5}
+        strokeColor="black"
+        mode="DRIVING"
       />
-    </View>
+      <Marker coordinate={origin} title={'Origin'} />
+      <Marker coordinate={destination} title={'Destination'} />
+    </MapView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-});
 
 export default HomeScreen;
---
+++ src/utils/mocks/VehicleLocationMock.ts
@@ -0,0 +1,265 @@
+interface VehicleLocationType {
+  latitude: number;
+  longitude: number;
+}
+
+const VehicleLocationMock: Array<VehicleLocationType> = [
+  {
+    latitude: -18.914931,
+    longitude: -48.244491,
+  },
+  {
+    latitude: -18.915202,
+    longitude: -48.244386,
+  },
+  {
+    latitude: -18.915261,
+    longitude: -48.244156,
+  },
+  {
+    latitude: -18.915306,
+    longitude: -48.243990,
+  },
+  {
+    latitude: -18.915351,
+    longitude: -48.243829,
+  },
+  {
+    latitude: -18.915404,
+    longitude: -48.243645,
+  },
+  {
+    latitude: -18.915439,
+    longitude: -48.243513,
+  },
+  {
+    latitude: -18.915480,
+    longitude: -48.243373,
+  },
+  {
+    latitude: -18.915514,
+    longitude: -48.243241,
+  },
+  {
+    latitude: -18.915629,
+    longitude: -48.243253,
+  },
+  {
+    latitude: -18.915719,
+    longitude: -48.243285,
+  },
+  {
+    latitude: -18.915842,
+    longitude: -48.243332,
+  },
+  {
+    latitude: -18.915973,
+    longitude: -48.243372,
+  },
+  {
+    latitude: -18.915973,
+    longitude: -48.243372,
+  },
+  {
+    latitude: -18.916049,
+    longitude: -48.243392,
+  },
+  {
+    latitude: -18.916088,
+    longitude: -48.243288,
+  },
+  {
+    latitude: -18.916120,
+    longitude: -48.243143,
+  },
+  {
+    latitude: -18.916167,
+    longitude: -48.242993,
+  },
+  {
+    latitude: -18.916215,
+    longitude: -48.242817,
+  },
+  {
+    latitude: -18.916247,
+    longitude: -48.242677,
+  },
+  {
+    latitude: -18.916275,
+    longitude: -48.242543,
+  },
+  {
+    latitude: -18.916322,
+    longitude: -48.242381,
+  },
+  {
+    latitude: -18.916369,
+    longitude: -48.242151,
+  },
+  {
+    latitude: -18.916397,
+    longitude: -48.241999,
+  },
+  {
+    latitude: -18.916451,
+    longitude: -48.241831,
+  },
+  {
+    latitude: -18.916498,
+    longitude: -48.241657,
+  },
+  {
+    latitude: -18.916555,
+    longitude: -48.241440,
+  },
+  {
+    latitude: -18.916589,
+    longitude: -48.241317,
+  },
+  {
+    latitude: -18.916646,
+    longitude: -48.241080,
+  },
+  {
+    latitude: -18.916682,
+    longitude: -48.240917,
+  },
+  {
+    latitude: -18.916715,
+    longitude: -48.240777,
+  },
+  {
+    latitude: -18.916734,
+    longitude: -48.240678,
+  },
+  {
+    latitude: -18.916819,
+    longitude: -48.240704,
+  },
+  {
+    latitude: -18.916958,
+    longitude: -48.240747,
+  },
+  {
+    latitude: -18.916959,
+    longitude: -48.240745,
+  },
+  {
+    latitude: -18.917252,
+    longitude: -48.240832,
+  },
+  {
+    latitude: -18.917324,
+    longitude: -48.240849,
+  },
+  {
+    latitude: -18.917477,
+    longitude: -48.240897,
+  },
+  {
+    latitude: -18.917598,
+    longitude: -48.240929,
+  },
+  {
+    latitude: -18.917690,
+    longitude: -48.240956,
+  },
+  {
+    latitude: -18.917756,
+    longitude: -48.240976,
+  },
+  {
+    latitude: -18.917852,
+    longitude: -48.241005,
+  },
+  {
+    latitude: -18.917988,
+    longitude: -48.241044,
+  },
+  {
+    latitude: -18.918167,
+    longitude: -48.241094,
+  },
+  {
+    latitude: -18.918271,
+    longitude: -48.241121,
+  },
+  {
+    latitude: -18.918365,
+    longitude: -48.241147,
+  },
+  {
+    latitude: -18.918513,
+    longitude: -48.241191,
+  },
+  {
+    latitude: -18.918557,
+    longitude: -48.241201,
+  },
+  {
+    latitude: -18.918653,
+    longitude: -48.241233,
+  },
+  {
+    latitude: -18.918839,
+    longitude: -48.241277,
+  },
+  {
+    latitude: -18.918880,
+    longitude: -48.241291,
+  },
+  {
+    latitude: -18.918943,
+    longitude: -48.241307,
+  },
+  {
+    latitude: -18.918967,
+    longitude: -48.241317,
+  },
+  {
+    latitude: -18.919096,
+    longitude: -48.241352,
+  },
+  {
+    latitude: -18.919156,
+    longitude: -48.241367,
+  },
+  {
+    latitude: -18.919296,
+    longitude: -48.241404,
+  },
+  {
+    latitude: -18.919425,
+    longitude: -48.241444,
+  },
+  {
+    latitude: -18.919474,
+    longitude: -48.241457,
+  },
+  {
+    latitude: -18.919569,
+    longitude: -48.241487,
+  },
+  {
+    latitude: -18.919804,
+    longitude: -48.241546,
+  },
+  {
+    latitude: -18.919905,
+    longitude: -48.241574,
+  },
+  {
+    latitude: -18.920058,
+    longitude: -48.241612,
+  },
+  {
+    latitude: -18.920189,
+    longitude: -48.241814,
+  },
+  {
+    latitude: -18.920153,
+    longitude: -48.241946,
+  },
+];
+
+export default VehicleLocationMock;
