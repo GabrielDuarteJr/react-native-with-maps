5e7db01c83be314720254b8c657c3bd39778eba9
Gabriel Duarte Jr <gabriel181193@gmail.com>
2023-01-08 18:27:49 -0300

    chore: modified home screen

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,68 +1,29 @@
-import React, {useEffect, useState, useMemo} from 'react';
-import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
-import MapViewDirections from 'react-native-maps-directions';
-import VehicleLocationMock from '~/utils/mocks/VehicleLocationMock';
+import React, {useContext} from 'react';
+import {CarContext} from '~/contexts/CarContext';
+import NavigationMapComponent from '~/components/NavigationMapComponent/NavigationMapComponent';
+import CloseButtonComponent from '~/components/CloseButtonComponent/CloseButtonComponent';
+import InfoViewComponent from '~/components/InfoViewComponent/InfoViewComponent';
+import DestinationMock from '~/utils/mocks/DestinationMock';
+import {Container} from './styles';
 
-const GOOGLE_MAPS_APIKEY = '######################';
-
-const HomeScreen = () => {
-  const [origin, setOrigin] = useState(VehicleLocationMock[0]);
-  const [originItemIndex, setOriginItemIndex] = useState(0);
-
-  const destination = {
-    latitude: -18.920112,
-    longitude: -48.242071,
-  };
-
-  const originLoc = useMemo(
-    () => ({
-      latitude: origin.latitude,
-      longitude: origin.longitude,
-    }),
-    [origin],
-  );
-
-  const destinationLoc = {
-    latitude: destination.latitude,
-    longitude: destination.longitude,
-  };
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (originItemIndex < VehicleLocationMock.length) {
-        const newOrigin = VehicleLocationMock[originItemIndex];
-        setOrigin(newOrigin);
-        setOriginItemIndex(originItemIndex + 1);
-      } else {
-        clearInterval(interval);
-      }
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, [originItemIndex]);
+const HomeScreen: React.FunctionComponent = () => {
+  const {origin, carOrientation, carDistance, carTime, onRestart} =
+    useContext(CarContext);
 
   return (
-    <MapView
-      style={{width: '100%', height: '100%'}}
-      provider={PROVIDER_GOOGLE}
-      showsUserLocation={true}
-      initialRegion={{
-        latitude: -18.920112,
-        longitude: -48.242071,
-        latitudeDelta: 0.0222,
-        longitudeDelta: 0.0121,
-      }}>
-      <MapViewDirections
-        origin={originLoc}
-        destination={destinationLoc}
-        apikey={GOOGLE_MAPS_APIKEY}
-        strokeWidth={5}
-        strokeColor="black"
-        mode="DRIVING"
+    <Container>
+      <NavigationMapComponent
+        origin={origin}
+        destination={DestinationMock}
+        carOrientation={carOrientation}
       />
-      <Marker coordinate={origin} title={'Origin'} />
-      <Marker coordinate={destination} title={'Destination'} />
-    </MapView>
+      <CloseButtonComponent onClose={onRestart} />
+      <InfoViewComponent
+        onCancel={onRestart}
+        distance={carDistance}
+        time={carTime}
+      />
+    </Container>
   );
 };
 
