41486c62b307794188185f6401ef5a060b7775ce
Gabriel Duarte Jr <gabriel181193@gmail.com>
2023-01-08 18:19:01 -0300

    chore: color file created

---
+++ src/utils/colors.ts
@@ -0,0 +1,11 @@
+const colors = {
+  blue: '#3A5DFB',
+  white: '#FFFFFF',
+  darkBlue: '#242E42',
+  blueLight: '#C8C7CC',
+  grayLight: '#F7F7F7',
+  grayIntermediate: '#EFEFF4',
+  green: '#4CE5B1',
+};
+
+export default colors;
